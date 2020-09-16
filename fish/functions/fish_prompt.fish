128b040518aca982135806914cb9643136a3b91c
Enrico Borba <enricozb@gmail.com>
2020-09-16 07:01:15 -1000

    sync

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,17 +1,31 @@
 function fish_prompt
-  set_color purple
+  set -l _status $status
+
+  set_color magenta
   echo -n "["
 
   set_color red
   echo -n (whoami)
 
+  set_color green
+  echo -n "@"
+
+  set_color cyan
+  echo -n (hostname)
+
   set_color white
-  echo -n " : "
+  echo -n " "
 
   set_color green
   echo -n (prompt_pwd)
 
-  set_color purple
+  if [ $_status -ne 0 ]
+    set_color -o red
+    echo -n " $_status "
+    set_color normal
+  end
+
+  set_color magenta
   echo -n "] "
 
   set_color normal
