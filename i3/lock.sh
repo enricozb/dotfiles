9a83223de9ec57acb86f96bbfe938c4551abd325
Enrico Borba <enricozb@gmail.com>
2020-10-05 14:43:11 -1000

    enricozb@xibalba: track /home/enricozb/.config/i3/lock.sh --name i3

---
+++ i3/lock.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+green="29BF12"
+yellow="FF9914"
+red="F21B3F"
+
+i3lock \
+  --ring-width=4 \
+  --pass-media-keys \
+  --separatorcolor=00000000 \
+  --linecolor=00000000 \
+  --radius=200 \
+  --ringcolor=00000000 \
+  --keyhlcolor=${green}ff \
+  --bshlcolor=${red}ff \
+  --insidecolor=252525bb \
+  --verifcolor=00000000 \
+  --insidevercolor=${yellow}99 \
+  --ringvercolor=${yellow}99 \
+  --wrongcolor=00000000 \
+  --insidewrongcolor=${red}99 \
+  --ringwrongcolor=${red}99 \
+  --image "/home/enricozb/.wallpapers/e5.1/Carmine De Fazio-2560x1440.png"
