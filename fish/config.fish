f8af78f47b0006de7226d2a9b03f33b2558de5b3
Enrico Borba <enricozb@gmail.com>
2021-05-19 16:00:27 -1000

    enricozb@xibalba: sync

--- fish/config.fish
+++ fish/config.fish
@@ -29,14 +29,13 @@ set -x PATH $PATH "/usr/bin/vendor_perl/"   # exiftool
 
 
 # ------------------------ aliases -----------------------
-alias clear "tput clear"
-alias cp    "cp -p"
-alias gg    "lazygit"
-alias l     "ls"
-alias o     "open"
-alias tree  "tree -C"
-alias wg    "wiki_grep"
-alias wiki  "kak ~/wiki/_.md"
+alias o "open"
+alias l "ls"
+alias cp "cp -p"
+alias wiki "kak ~/wiki/_.md"
+alias wg "wiki_grep"
+alias gg "lazygit"
+alias tree "tree -C"
 
 
 # ----------------------- functions ----------------------
